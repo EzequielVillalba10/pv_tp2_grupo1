374df2fe37da18f270b3cb3aa5ba2dd4b4406517
Facundo Pol <facundopol24@gmail.com>
2025-04-11 19:22:10 -0300

    ejercicio 5 - FacundoPol

---
+++ src/ejercicio5.js
@@ -0,0 +1,8 @@
+// EJERCICO 5
+let horas=2;
+let minutos=30;
+
+let segundos=horas*60*60+minutos*60;
+    console.log("Hora: "+horas+":"+minutos);
+    console.log("Total Segundos: "+segundos);
+let num1;
