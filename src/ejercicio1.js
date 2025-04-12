c2c17d362de6e8fbbb72a5631f453d175ae84090
Ezequiel Villalba <ezequielvillagf@gmail.com>
2025-04-12 12:31:42 -0300

    Ejercicio 1 - Ezequiel Vilalba

---
+++ src/ejercicio1.js
@@ -0,0 +1,7 @@
+const integrantes = "Ezequiel Villalba, Sofía Ramos, Geraldo Aramayo, Miguel Angel Abregu y Facundo Pol";
+
+const mostrarMensaje = () => {
+  console.log("Somos " + integrantes + " y estamos aprendiendo JavaScript.");
+};
+
+mostrarMensaje();
