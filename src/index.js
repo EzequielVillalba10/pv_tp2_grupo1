66ae7a1026ef79e3e54670b99b39c73b732a68d7
Gerardo <leon.capo2000@gmail.com>
2025-04-13 11:43:26 -0300

    ejercicio 3 completado por Aramayo Gerardo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-console.log("Hello Word!");
-console.log("Esto es PV!");
+const pi = 3.14;
+let radio=7.5;
+let area=pi*Math.pow(radio,2);
+console.log("el area del circulo con radio "+radio + " es: "+area);
