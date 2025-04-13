f32f38fc4fa7830fdb0eb410101711dc9531f716
Ezequiel Villalba <ezequielvillagf@gmail.com>
2025-04-13 17:39:51 -0300

    Ejercicio 16 - Cambie la funcion tradicional por la funcion flecha - Ezequiel Villalba

--- src/ejercicio16.js
+++ src/ejercicio16.js
@@ -1,22 +1,18 @@
-function sumarRango(inicio,fin)
-{
-    if (fin<inicio)
-    {
+const sumarRango = (inicio, fin) => {
+    if (fin < inicio) {
         alert("Error, el valor fin no puede ser menor que el valor numerico inicio!");
-        return 0; 
-    }
-    else
-    {
+        return 0;
+    } else {
         let suma = 0;
         for (let i = inicio; i <= fin; i++) {
-        suma += i;
+            suma += i;
+        }
+        return suma;
     }
-    return suma;
-    }
-}
+};
 
 let inicio = parseInt(prompt("Ingrese el valor inicial: "));
 let fin = parseInt(prompt("Ingrese el valor final: "));
 
-let suma=sumarRango(inicio,fin);
+let suma = sumarRango(inicio, fin);
 alert("La suma de los valores entre esos rangos es: " + suma);
