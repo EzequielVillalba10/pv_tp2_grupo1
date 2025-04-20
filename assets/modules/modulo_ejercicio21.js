d66378d0d31c3c5b3a5bdd65a0186d464470c416
Miguel Angel Abregu <miguel_af_555@hotmail.com>
2025-04-20 13:29:44 -0300

    ejercicio 21 Miguel Abregu

---
+++ assets/modules/modulo_ejercicio21.js
@@ -0,0 +1,6 @@
+// funcion para actualizar el texto del parrafo
+
+export function actualizarTexto(texto) {  
+    const resultado = document.getElementById("resultado");  
+    resultado.textContent = texto;  
+}  
